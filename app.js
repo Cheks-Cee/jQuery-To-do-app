83a9c4ec9582991bac2abccdfaa1209059ea43ee
Helen Collins-Eromosele <helenokonicha@gmail.com>
2020-01-21 04:29:44 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+$('ul').on('click', 'li', function(){
+    $(this).toggleClass('done');
+})
+
+$('ul').on('click', 'span', function(event){
+    $(this).parent().remove();
+    event.stopPropagation();
+})
+
+$("input[type='text']").on('keypress', function(event){
+    // define to-do value 
+    let todoItem = $(this).val();
+    
+    if(event.which===13){
+        // clear the input area 
+        $(this).val("");
+        // add the value of the text entered to the to-do items
+        console.log(todoItem);
+        $('ul').append('<li><span><i class="fas fa-trash-alt"></i></span><i class="fas fa-check"></i>' + todoItem + '</li>')
+    }
+})
